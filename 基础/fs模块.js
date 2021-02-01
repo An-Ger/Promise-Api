061955258c381f4e530cfcec830d2715f3d2764c
an-ger <aiboowen@gmail.com>
2021-02-01 13:41:50 +0800

    Promise封装fs操作

---
+++ 基础/fs模块.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const { resolve } = require("path");
+// fs.readFile('./resource/1.txt', (err, data) => {
+//     if (err) throw err;
+//     console.log(data.toString());
+// })
+//Promise
+let p = new Promise((resolve, reject) => {
+  fs.readFile("./resource/1.txt", (err, data) => {
+    if (err) reject(err);
+    resolve(data);
+  });
+});
+p.then(
+  (value) => {
+    console.log(value.toString());
+  },
+  (reason) => {
+    console.log(reason);
+  }
+);
