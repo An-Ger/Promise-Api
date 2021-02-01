f4a0f87912b263b5164fbcb7d6d7f4d926b1ebb6
an-ger <aiboowen@gmail.com>
2021-02-01 13:58:14 +0800

    Promise封装fs读取文件操作

---
+++ 基础/封装fs读取文件操作.js
@@ -0,0 +1,9 @@
+function mineReadFile (path) {
+    return new Promise((resolve, reject) => {
+        require('fs').readFile(path, (err, data) => {
+            if(err) reject(err)
+            resolve(data)
+        })
+    })
+}
+mineReadFile('./resource/1.txt').then(value => console.log(value.toString()), reason => console.log(reason))
